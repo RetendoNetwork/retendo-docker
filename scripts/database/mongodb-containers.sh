c88b58273ec27af12898eac17129532f639169b5
00cedke <mathiskera2509@gmail.com>
2024-10-19 16:48:55 +0200

    Added MongoDB Retendo

--- scripts/database/mongodb-containers.sh
+++ scripts/database/mongodb-containers.sh
@@ -4,15 +4,13 @@
 source "$(dirname "$(realpath "$0")")/../framework.sh"
 parse_arguments "$@"
 
-print_info "Setting up MongoDB container..."
-
 mongodb_init_script=$(cat "$git_base_dir/scripts/database/mongodb-init.js")
 
 compose_no_progress up -d mongodb
 
-run_command_until_success "Waiting for MongoDB to be ready..." 10 \
+run_command_until_success "Awaiting MongoDB to become ready.." 10 \
     docker compose exec mongodb mongosh --eval "db.adminCommand('ping')"
 
 run_verbose docker compose exec mongodb mongosh --eval "$mongodb_init_script"
 
-print_success "MongoDB container is set up."
+print_success "MongoDB container has been set up."
--- setup.sh
+++ setup.sh
@@ -80,8 +80,11 @@ print_stage "Check updates from all submodules."
 print_stage "Setting up all containers."
 ./scripts/server-containers.sh
 
-print_stage "Building all server config"
+print_stage "Building all server config."
 ./scripts/setup-building-config.sh
+
+print_stage "Creating all Retendo Database."
+./scripts/database/mongodb-containers.sh
 
 print_stage "Starting the Retendo Network server."
 docker compose up -d --build
