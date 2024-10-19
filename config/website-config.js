1d56dbce8027640ef9ccc97c820c64eca3dda709
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-10-19 22:09:09 +0200

    Update website-config.js

--- config/website-config.js
+++ config/website-config.js
@@ -5,6 +5,13 @@ const config = {
         port: 82,
         url: "retendo.online"
     },
+
+    db: {
+        "connection_string": "mongodb://127.0.0.1:27017/retendo_account?replicaSet=rs",
+        "options": {
+            "useNewUrlParser": true
+        }
+    }
 };
 
 const jsonConfig = JSON.stringify(config, null, 2);
