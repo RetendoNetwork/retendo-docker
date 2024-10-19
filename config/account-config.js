ec8f80c25177e206d61eb33a86bb985d0c924c20
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-10-19 22:08:54 +0200

    Update account-config.js

--- config/account-config.js
+++ config/account-config.js
@@ -4,6 +4,13 @@ const config = {
     config: {
         port: 83
     },
+
+    db: {
+        "connection_string": "mongodb://127.0.0.1:27017/retendo_account?replicaSet=rs",
+        "options": {
+            "useNewUrlParser": true
+        }
+    }
 };
 
 const jsonConfig = JSON.stringify(config, null, 2);
